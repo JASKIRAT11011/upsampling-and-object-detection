50574b13abde6b4aad875c1d7fc5c08fa3667345
Jaskirat <0001JASKIRAT@gmail.com>
2023-06-25 14:55:02 +0530

    Initial commit

---
+++ PROJECT-UPSAMPLING AND OBJECT DETECTION/upsampling/WDSR/model/model1.py
@@ -0,0 +1,14 @@
+import numpy as np
+import tensorflow as tf
+
+def resolve_single(model, lr):
+    return resolve(model, tf.expand_dims(lr, axis=0))[0]
+
+
+def resolve(model, lr_batch):
+    lr_batch = tf.cast(lr_batch, tf.float32)
+    sr_batch = model(lr_batch)
+    sr_batch = tf.clip_by_value(sr_batch, 0, 255)
+    sr_batch = tf.round(sr_batch)
+    sr_batch = tf.cast(sr_batch, tf.uint8)
+    return sr_batch
